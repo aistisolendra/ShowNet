c914410676d22f66582a546636921374026ca9f1
Aistis Olendra <aistis3211@gmail.com>
2022-02-14 16:47:28 +0200

    Add interfaces view logic, support for localization

--- ViewModels/InterfacesViewModel.cs
+++ ViewModels/InterfacesViewModel.cs
@@ -1,12 +1,38 @@
-﻿using Caliburn.Micro;
-using ShowNet.Services;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Caliburn.Micro;
+using ShowNet.Services.Interfaces;
 
 namespace ShowNet.ViewModels;
 
-public class InterfacesViewModel : Screen
+public class InterfacesViewModel : Screen, INotifyPropertyChanged
 {
+    private readonly InterfacesFinder _interfacesFinder;
+
     public InterfacesViewModel(InterfacesFinder interfacesFinder)
     {
-        
+        _interfacesFinder = interfacesFinder;
+
+        Interfaces = new ObservableCollection<Interface>(_interfacesFinder.GetInterfaces());
+    }
+
+    private ObservableCollection<Interface> _interfaces = new();
+
+    public ObservableCollection<Interface> Interfaces
+    {
+        get => _interfaces;
+        set
+        {
+            _interfaces = value;
+            OnPropertyChanged(nameof(Interfaces));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
     }
 }
