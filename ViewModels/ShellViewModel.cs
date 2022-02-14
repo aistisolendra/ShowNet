3f1f38df39a95120b33db971d37aeed7dedf4321
Aistis Olendra <aistis3211@gmail.com>
2022-02-14 15:17:31 +0200

    Add ShellViewModel for Caliburn.Micro Startup

--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,8 +1,17 @@
 ﻿using Caliburn.Micro;
 
-namespace ShowNet.ViewModels
+namespace ShowNet.ViewModels;
+
+public class ShellViewModel : Conductor<Screen>
 {
-    public class ShellViewModel : PropertyChangedBase
+    public ShellViewModel(MainViewModel mainViewModel, InterfacesViewModel interfacesViewModel)
     {
+        ChangeView(interfacesViewModel);
+    }
+
+    private async void ChangeView(Screen screen)
+    {
+        await ActivateItemAsync(screen)
+            .ConfigureAwait(false);
     }
 }
