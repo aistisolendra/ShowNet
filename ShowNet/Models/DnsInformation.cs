c76c3bd131cf57aee96e2acc11bb212a9badc6ad
Aistis Olendra <aistis3211@gmail.com>
2022-02-15 02:20:45 +0200

    Add static DNS information class

---
+++ ShowNet/Models/DnsInformation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ShowNet.Models
+{
+    public enum DnsHosts
+    {
+        Google,
+        Cloudflare
+    }
+
+    public class DnsInformation
+    {
+        public static Dictionary<DnsHosts, List<string>> AllDns = new()
+        {
+            {DnsHosts.Google, new List<string> {"8.8.8.8", "8.8.4.4"}},
+            {DnsHosts.Cloudflare, new List<string> {"1.1.1.1", "1.0.0.1"}}
+        };
+    }
+}
