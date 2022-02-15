8b4774db5c7180d08ec6b8f10ece948d5a543caa
Aistis Olendra <aistis3211@gmail.com>
2022-02-15 15:15:15 +0200

    Add local as DNS

--- ShowNet/Models/DnsInformation.cs
+++ ShowNet/Models/DnsInformation.cs
@@ -5,15 +5,17 @@ namespace ShowNet.Models
     public enum DnsHosts
     {
         Google,
-        Cloudflare
+        Cloudflare,
+        Local
     }
 
     public class DnsInformation
     {
-        public static Dictionary<DnsHosts, List<string>> AllDns = new()
+        public static Dictionary<DnsHosts, string[]?> AllDns = new()
         {
-            {DnsHosts.Google, new List<string> {"8.8.8.8", "8.8.4.4"}},
-            {DnsHosts.Cloudflare, new List<string> {"1.1.1.1", "1.0.0.1"}}
+            {DnsHosts.Google, new[] {"8.8.8.8", "8.8.4.4"}},
+            {DnsHosts.Cloudflare, new[] {"1.1.1.1", "1.0.0.1"}},
+            {DnsHosts.Local, null}
         };
     }
 }
